98cbe851c37a26ae61ffd8f1ed4ec6cc3878ea26
a5768549 <44581221+a5768549@users.noreply.github.com>
2021-06-08 22:53:36 +0800

    fix something

--- app/app.js
+++ app/app.js
@@ -1,43 +1,49 @@
-'use strict';
+"use strict";
 
-angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
+angular
+    .module("ethExplorer", ["ngRoute", "ui.bootstrap"])
 
-.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'mainCtrl'
-            }).
-            when('/block/:blockId', {
-                templateUrl: 'views/blockInfos.html',
-                controller: 'blockInfosCtrl'
-            }).
-            when('/transaction/:transactionId', {
-                templateUrl: 'views/transactionInfos.html',
-                controller: 'transactionInfosCtrl'
-            }).
-            when('/address/:addressId', {
-                templateUrl: 'views/addressInfo.html',
-                controller: 'addressInfoCtrl'
-            }).
-            otherwise({
-                redirectTo: '/'
-            });
-    }])
-    .run(function($rootScope) {
+    .config([
+        "$routeProvider",
+        function ($routeProvider) {
+            $routeProvider
+                .when("/", {
+                    templateUrl: "views/main.html",
+                    controller: "mainCtrl",
+                })
+                .when("/block/:blockId", {
+                    templateUrl: "views/blockInfos.html",
+                    controller: "blockInfosCtrl",
+                })
+                .when("/transaction/:transactionId", {
+                    templateUrl: "views/transactionInfos.html",
+                    controller: "transactionInfosCtrl",
+                })
+                .when("/address/:addressId", {
+                    templateUrl: "views/addressInfo.html",
+                    controller: "addressInfoCtrl",
+                })
+                .otherwise({
+                    redirectTo: "/",
+                });
+        },
+    ])
+    .run(function ($rootScope) {
         var web3 = new Web3();
         var host = window.location.hostname;
-        var eth_node_url = 'http://' + host + ':8545';
-	web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
+        var eth_node_url = "http://" + host + ":8545";
+        web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
         $rootScope.web3 = web3;
-        function sleepFor( sleepDuration ){
+        $rootScope.abiDecoder = abiDecoder;
+        function sleepFor(sleepDuration) {
             var now = new Date().getTime();
-            while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
+            while (new Date().getTime() < now + sleepDuration) {
+                /* do nothing */
+            }
         }
         var connected = false;
-        if(!web3.isConnected()) {
-            $('#connectwarning').modal({keyboard:false,backdrop:'static'})
-            $('#connectwarning').modal('show')
+        if (!web3.isConnected()) {
+            $("#connectwarning").modal({ keyboard: false, backdrop: "static" });
+            $("#connectwarning").modal("show");
         }
     });
